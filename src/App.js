9441bba064481f514578b430ae0c64cbddc6b45c
will <thewillboesetrio@gmail.com>
2023-02-25 19:21:02 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [savedSession, setSavedSession] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lap Counter Exercise v1</h1>
+      <div>
+        <h3>LAP COUNT</h3>
+        <h2 className="counter">{count}</h2>
+        <button className="button-19" onClick={() => setCount(count + 1)}>INCREMENT</button>
+        <button className="button-19" onClick={() => setCount(count - 1)} disabled={count <= 0}>DECREMENT</button>
+        <div>   
+        <button className="button-19" onClick={() => setCount(0)}>RESET</button>
+        <button className="button-19" onClick={() => {setSavedSession(count); setCount(0)}} disabled={count <= 0}>SAVE SESSION</button>
+        </div>
+        <h3>PREVIOUS SESSION: {savedSession}</h3>
+      </div>
     </div>
   );
 }
